6f496f88130793497124a5c2e12d20e40a5bb371
Максим Еманаков <emanakov.m@bk.ru>
2025-03-21 09:03:05 +0300

    add secure storage of tokens

--- TEST1.py
+++ TEST1.py
@@ -2,23 +2,15 @@ import json
 import requests
 from flask import Flask, render_template, redirect, url_for
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__, template_folder='templates')
 
-CONFIG_FILE = "config.json"
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-if not os.path.exists(CONFIG_FILE):
-    print(f"Ошибка: Файл {CONFIG_FILE} отсутствует. Создайте его вручную и добавьте ваш GitHub токен.")
-    exit(1)
-
-try:
-    with open(CONFIG_FILE) as config_file:
-        config = json.load(config_file)
-        if not config.get("github_token"):
-            print("Ошибка: Токен GitHub отсутствует в config.json!")
-            exit(1)
-except (FileNotFoundError, json.JSONDecodeError) as e:
-    print(f"Ошибка загрузки {CONFIG_FILE}: {e}")
+if not GITHUB_TOKEN:
+    print("Ошибка: Токен GitHub отсутствует! Укажите его в .env файле.")
     exit(1)
 
 cached_user_data = None
@@ -27,7 +19,7 @@ def fetch_github_user_data():
     global cached_user_data
     try:
         api_url = "https://api.github.com/user"
-        headers = {'Authorization': f'token {config["github_token"]}'}
+        headers = {'Authorization': f'token {GITHUB_TOKEN}'}
 
         response = requests.get(api_url, headers=headers)
 
@@ -45,7 +37,6 @@ def fetch_github_user_data():
             "name": user_data.get("name", "Не указано"),
             "login": user_data.get("login", "Не указано"),
             "email": user_data.get("email", "Не указано"),
-            "profile_url": user_data.get("html_url", "#"),
             "public_repos": user_data.get("public_repos", "Не указано"),
             "followers": user_data.get("followers", "Не указано"),
             "created_at": user_data.get("created_at", "Не указано")
@@ -65,13 +56,11 @@ def resume():
         cached_user_data = fetch_github_user_data()
     return render_template('resume_template.html', user=cached_user_data)
 
-
 @app.route('/update')
 def update():
     global cached_user_data
     cached_user_data = fetch_github_user_data()
     return redirect(url_for('resume'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
